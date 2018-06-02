d6108c945ea3ff3da806084a363e0835ad8facc4
Mary Jackson <jackson.marytherese@gmail.com>
2018-06-02 14:04:34 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,11 +7,13 @@ class Anagram
   end
 
   def self.match(words)
+    matched_words = []
     words_to_match = %w(words)
     words_to_match.each do |word|
       if word.length == @maybe_anagram.length
         if word.split.sort == @maybe_anagram.split.sort
           word
+        end
 
   end
 end
