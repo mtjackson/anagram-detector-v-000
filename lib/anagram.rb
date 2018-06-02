3feb2064fc820eab0942009917c73ea8fc16b8bc
Mary Jackson <jackson.marytherese@gmail.com>
2018-06-02 13:58:31 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,12 +1,13 @@
 class Anagram
 
-  attr_accessor :word
+  attr_accessor :maybe_anagram
 
-  def initialize(word)
-    @word = word
+  def initialize(maybe_anagram)
+    @maybe_anagram = maybe_anagram
   end
 
   def self.match(words)
     words_to_match = %w(words)
+    words_to_match.each do |word|
   end
 end
