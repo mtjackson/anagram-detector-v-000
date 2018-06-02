7134248b61a9dee03f1c999ffe48b63682d6c282
Mary Jackson <jackson.marytherese@gmail.com>
2018-06-02 14:33:10 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
-# Your code goes here!
+class Anagram
+
+  attr_accessor :maybe_anagram
+
+  def initialize(maybe_anagram)
+    @maybe_anagram = maybe_anagram
+  end
+
+  def match(words)
+    matched_words = []
+
+    words.each do |word|
+      if word.length == @maybe_anagram.length
+        if word.split("").sort == @maybe_anagram.split("").sort
+          matched_words << word
+        end
+      end
+    end
+    matched_words
+
+  end
+end
